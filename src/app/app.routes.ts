5e4d5a7360edd89cee259e57c43d986bcc40030a
Jerome Mohanan <2962518+jero2rome@users.noreply.github.com>
2024-08-25 11:45:46 +0200

    chore: Update Angular router configuration
    The Angular router configuration has been updated to include a new route for the `PageNotFoundComponent` and to redirect the root path to the `FirstComponent`. This ensures that users are directed to the appropriate component when navigating the application.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'first-component', component: FirstComponent },
-  { path: 'second-component', component: SecondComponent }
+  { path: 'second-component', component: SecondComponent },
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
